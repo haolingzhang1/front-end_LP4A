93b3dad799623d263c86a683755a76cf05da8d0c
haolingzhang1 <55579125+haolingzhang1@users.noreply.github.com>
2019-11-07 20:20:31 +0100

    incrementer

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -15,13 +15,13 @@ export class CounterService {
     this.initialValue = [0, 0, 0];
   }
 
-  increment(position: number): number {
-    this.initialValue[position]++;
-    return this.initialValue[position];
+ increment(): Observable<Counter> {
+    this.httpClient.patch("https://lp4asgadot.herokuapp.com/counters/53.json",{"value" : 1}).subscribe();
+    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/53.json");
   }
 
   getCounterValue(id: number): Observable<Counter> {
-    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json")
+    return this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/53.json")
   }
 
 }

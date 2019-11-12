b18713b21f9c4c866810bcb6457e17c57c49d524
haolingzhang1 <55579125+haolingzhang1@users.noreply.github.com>
2019-11-12 22:12:38 +0100

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CounterService } from '../counter.service';
-
+import {CounterService} from '../counter.service';
 import { Counter } from '../counter';
+
 
 @Component({
   selector: 'app-home',
@@ -9,15 +9,21 @@ import { Counter } from '../counter';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  counters: Array<Counter>;
-
+  counters: Array<Counter> = [];
   constructor(private counterService: CounterService) { }
-  
+
   ngOnInit() {
-    this.counters = this.counterService.getCountersArray();
+    this.counterService.getCounters()
+    .subscribe((_counters) => {
+      _counters.forEach((_counter) => {
+        this.counters.push(_counter);
+      });
+    });
+    console.log(this.counters);
   }
 
-  reset(){
-    this.counterService.reset();
-  } 
+  reset() {
+        this.counterService.reset();
+    }
+
 }

c60fc1f0130cbdc9f19329208986c52812a4ad83
haolingzhang1 <55579125+haolingzhang1@users.noreply.github.com>
2019-11-12 22:10:44 +0100

    Update counter.component.ts

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,29 +1,38 @@
-    
 import { Component, OnInit, Input } from '@angular/core';
 import { CounterService } from '../counter.service';
 import { Observable } from 'rxjs';
 import { Counter } from '../counter';
 
 @Component({
-  selector: 'app-counter',
-  templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+    selector: 'app-counter',
+    templateUrl: './counter.component.html',
+    styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
-  @Input() id : number;
+
+    @Input() id : number;
   name: string;
   value: number;
 
-  constructor(public counterService: CounterService) { }
+    constructor(public counterService: CounterService) { }
 
-  ngOnInit() {
-    this.counterService.getCounter(this.id).subscribe(counter => {
-      this.name = counter.name;
-      this.value = counter.value;
+    ngOnInit() {
+
+    this.counterService.getCounterValue(this.id)
+    .subscribe(counter => {
+              this.value =  counter.value;
+              this.name = counter.name;
+              this.id=counter.id;
     });
-  }
-
-  async increment(){
-    this.value = await this.counterService.increment(this.id);
-  }
 }
+
+   increment() {
+       this.counterService.increment(this.id)
+       .subscribe(counter =>{
+       this.value=counter.value;
+       });
+    }
+
+
+      }
+    

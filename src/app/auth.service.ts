e0c1c3acf2ca7be58b274572e68b1d0094016698
haolingzhang1 <55579125+haolingzhang1@users.noreply.github.com>
2019-11-12 07:37:32 +0100

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -12,8 +12,8 @@ export class AuthService {
   // Create an observable of Auth0 instance of client
   auth0Client$ = (from(
     createAuth0Client({
-      domain: "dev-amrlr2e6.eu.auth0.com",
-      client_id: "edhbqQvi5LA6eQR5e7wS38hUD7fyA2bC",
+      domain: "zhlangular.eu.auth0.com",
+      client_id: "Af6IUegF6xKdXtgy1BA7Y3HRFGPy40G",
       redirect_uri: `${window.location.origin}`
     })
   ) as Observable<Auth0Client>).pipe(
@@ -111,7 +111,7 @@ export class AuthService {
     this.auth0Client$.subscribe((client: Auth0Client) => {
       // Call method to log out
       client.logout({
-        client_id: "edhbqQvi5LA6eQR5e7wS38hUD7fyA2bC",
+        client_id: "Af6IUegF6xKdXtgy1BA7Y3HRFGPy40G",
         returnTo: `${window.location.origin}`
       });
     });

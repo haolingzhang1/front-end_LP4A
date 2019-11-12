a06b78f1a428080a307dac591805f72a0e971195
haolingzhang1 <55579125+haolingzhang1@users.noreply.github.com>
2019-11-12 22:09:00 +0100

    Update counter-detail.component.ts

--- src/app/counter-detail/counter-detail.component.ts
+++ src/app/counter-detail/counter-detail.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-
-import { CounterService }  from '../counter.service';
 import { Counter } from '../counter';
+import { ActivatedRoute } from '@angular/router';
+import { CounterService } from '../counter.service';
+import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'app-counter-detail',
@@ -12,28 +11,32 @@ import { Counter } from '../counter';
   styleUrls: ['./counter-detail.component.css']
 })
 export class CounterDetailComponent implements OnInit {
-  counter: Counter;
 
+  counter: Counter;
   constructor(
-    private route: ActivatedRoute,
+    private route : ActivatedRoute,
     private counterService: CounterService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getCounter();
   }
 
-  getCounter() : void {
+  getCounter() : void{
     this.route.params.subscribe(
       params => {
-        this.counterService.getCounter(params['id'])
+        this.counterService.getCounterValue(params['id'])
           .subscribe(counter => this.counter = counter);
       }
     );
   }
 
-  async increment(){
-    this.counter.value = await this.counterService.increment(this.counter.id);
+  increment(){
+    this.counterService.increment(this.counter.id)
+    .subscribe(counter =>{
+    this.counter.value=counter.value;
+    });
   }
+
 }
